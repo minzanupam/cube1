d5415f081c2837cf008ea965a9764e505fdab442
Anupam Ashish Minz <anupamminz300am@gmail.com>
2024-12-26 22:52:07 +0530

    blank window

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,25 @@
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
 #include <iostream>
 
+#define WIDTH 1280
+#define HEIGHT 720
+
 int main() {
-	printf("hello world\n");
+	if (!glfwInit()) {
+		fprintf(stderr, "failed to init glfw\n");
+		return -1;
+	}
+	GLFWwindow *window = glfwCreateWindow(WIDTH, HEIGHT, "cube1", NULL, NULL);
+	glfwMakeContextCurrent(window);
+	if (glewInit() != GLEW_OK) {
+		fprintf(stderr, "failed to init glew\n");
+		return -1;
+	}
+	while (!glfwWindowShouldClose(window)) {
+		glClear(GL_COLOR_BUFFER_BIT);
+		glfwPollEvents();
+		glfwSwapBuffers(window);
+	}
 	return 0;
 }
