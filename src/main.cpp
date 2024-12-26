ab4fe5f6ba50d40bf0a8557f690661320e4ec4aa
Anupam Ashish Minz <anupamminz300am@gmail.com>
2024-12-26 23:01:28 +0530

    blank triangle

--- src/main.cpp
+++ src/main.cpp
@@ -5,7 +5,17 @@
 #define WIDTH 1280
 #define HEIGHT 720
 
+void GLAPIENTRY MessageCallback(GLenum source, GLenum type, GLuint id,
+								GLenum severity, GLsizei length,
+								const GLchar *message, const void *userParam) {
+	fprintf(stderr,
+			"GL CALLBACK: %s type = 0x%x, severity = 0x%x, message = %s\n",
+			(type == GL_DEBUG_TYPE_ERROR ? "** GL ERROR **" : ""), type,
+			severity, message);
+}
+
 int main() {
+	unsigned int VAO, VBO;
 	if (!glfwInit()) {
 		fprintf(stderr, "failed to init glfw\n");
 		return -1;
@@ -16,8 +26,27 @@ int main() {
 		fprintf(stderr, "failed to init glew\n");
 		return -1;
 	}
+	glEnable(GL_DEBUG_OUTPUT);
+	glDebugMessageCallback(MessageCallback, 0);
+
+	glGenVertexArrays(1, &VAO);
+	glGenBuffers(1, &VBO);
+	glBindVertexArray(VAO);
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+
+	float vertices[] = {
+		-1.0f, -1.0f, 0.0f, ///
+		0.0f,  1.0f,  0.0f, ///
+		1.0f,  -1.0f, 0.0f, ///
+	};
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), NULL);
+	glEnableVertexAttribArray(0);
+
 	while (!glfwWindowShouldClose(window)) {
 		glClear(GL_COLOR_BUFFER_BIT);
+		glDrawArrays(GL_TRIANGLES, 0, 3);
 		glfwPollEvents();
 		glfwSwapBuffers(window);
 	}
