69629d8913bf77276e8dc9395c4e17e961fdc7e1
anupam <anupamminz300am@gmail.com>
2025-01-02 20:00:05 +0530

    refactor phong shader

--- src/shaders/fragment_phong.glsl
+++ src/shaders/fragment_phong.glsl
@@ -25,12 +25,12 @@ uniform Light light;
 
 void main() {
 	vec3 light_direction = normalize(light.position - frag_pos);
-	vec3 normal = normalize(frag_nor);
+	vec3 normal_direction = normalize(frag_nor);
 	vec3 view_direction = normalize(camera_pos - frag_pos);
-	vec3 reflection_direction = reflect(-light_direction, normal);
+	vec3 reflection_direction = reflect(-light_direction, normal_direction);
 
 	vec3 ambient = light.ambient * material.ambient;
-	float diff = max(dot(normal, light_direction), 0.0);
+	float diff = max(dot(normal_direction, light_direction), 0.0);
 	vec3 diffuse = light.diffuse * (diff * material.diffuse);
 	float spec = pow(max(dot(view_direction, reflection_direction), 0.0), material.shininess);
 	vec3 specular = light.specular * spec * material.specular;
