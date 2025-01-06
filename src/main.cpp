458775157b499a1e0e3e4b2c0aa1f56bf6360b58
anupam <anupamminz300am@gmail.com>
2025-01-06 10:45:07 +0530

    render and fix teapot position

--- src/main.cpp
+++ src/main.cpp
@@ -330,6 +330,8 @@ int main() {
 		glBindVertexArray(VAO_asset);
 		glBindBuffer(GL_ARRAY_BUFFER, VBO_asset);
 
+		model = glm::mat4(1.0f);
+		model = glm::translate(model, glm::vec3(0.0f, 0.1f, 0.0f));
 		glUniformMatrix4fv(u_Model, 1, GL_FALSE, glm::value_ptr(model));
 		glUniformMatrix4fv(u_View, 1, GL_FALSE, glm::value_ptr(view));
 		glUniformMatrix4fv(u_Projection, 1, GL_FALSE,
@@ -359,13 +361,13 @@ int main() {
 							  (void *)(3 * sizeof(float)));
 		glEnableVertexAttribArray(0);
 		glEnableVertexAttribArray(1);
-		// glDrawArrays(GL_TRIANGLES, 0, 3 * asset_triangle_count);
+		glDrawArrays(GL_TRIANGLES, 0, 3 * asset_triangle_count);
 
 		glBindVertexArray(VAO_ground);
 		glBindBuffer(GL_ARRAY_BUFFER, VBO_ground);
 
 		model = glm::scale(glm::mat4(1.0f), glm::vec3(10.0f, 0.1f, 10.0f));
-		model = glm::translate(model, glm::vec3(0.0f, 0.1f, 0.0f));
+		model = glm::translate(model, glm::vec3(0.0f, -0.05f, 0.0f));
 		glUniformMatrix4fv(u_Model, 1, GL_FALSE, glm::value_ptr(model));
 		glUniformMatrix4fv(u_View, 1, GL_FALSE, glm::value_ptr(view));
 		glUniformMatrix4fv(u_Projection, 1, GL_FALSE,
