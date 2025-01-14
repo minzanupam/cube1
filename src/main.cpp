7c39fa104964f668034d890c142448f10649db41
anupam <anupamminz300am@gmail.com>
2025-01-14 21:29:01 +0530

    remove incorrect bind texture call

--- src/main.cpp
+++ src/main.cpp
@@ -323,7 +323,6 @@ int main() {
 				 GL_UNSIGNED_BYTE, NULL);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-	glBindTexture(GL_TEXTURE_2D, 0);
 	glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D,
 						   texture, 0);
 	glGenRenderbuffers(1, &RBO);
