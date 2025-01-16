13b2799a85dea0c47924eb3e20881de4a7c5375d
anupam <anupamminz300am@gmail.com>
2025-01-16 10:52:11 +0530

    post processing invert color

--- src/shaders/fragment_quad.glsl
+++ src/shaders/fragment_quad.glsl
@@ -7,5 +7,5 @@ uniform sampler2D screenTexture;
 out vec4 FragColor;
 
 void main() {
-	FragColor = texture(screenTexture, TexCoord);
+	FragColor = vec4(vec3(1.0 - texture(screenTexture, TexCoord)), 1.0);
 }
